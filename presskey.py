d950dc0de57afa627c45daf8ec0197c8d7c19c9c
seunghyun choi <hannibal38@naver.com>
2022-05-02 12:09:39 +0900

    presskey-comment

--- presskey.py
+++ presskey.py
@@ -13,11 +13,10 @@ win_x1,win_y1,win_x2,win_y2=win32gui.GetWindowRect(handle)
 print("Resolution: " + str(size))
 print("Target window position: " + str(win_x1) + "," + str(win_y1) + " ~ " + str(win_x2) + "," + str(win_y2))
 print("MousePosition Before: " + str(pyautogui.position()))
-
+# move mouse position to app's x1,y1 and move +100,+100
 pyautogui.moveTo(win_x1,win_y1)
 pyautogui.moveRel(100,100)
 print("MousePosition After: " + str(pyautogui.position()))
-
 
 # Press quit
 #win32api.SendMessage(edit, win32con.WM_CHAR, ord('q'), 0)
