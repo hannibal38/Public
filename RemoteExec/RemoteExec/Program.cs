b12b23b5767c16bfc87d8cc765bb38b74ccf83d1
seunghyun choi <hannibal38@naver.com>
2022-09-08 13:45:47 +0900

    remoteexec_2

--- RemoteExec/RemoteExec/Program.cs
+++ RemoteExec/RemoteExec/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +9,41 @@ namespace RemoteExec
 {
     class Program
     {
-        /* D:\SysinternalsSuite_20181210>psexec \\172.21.150.190 -nobanner cmd /c query session
+    /* D:\SysinternalsSuite_20181210>psexec \\172.21.150.190 -nobanner cmd /c query session | findstr Active
+    Starting cmd on 172.21.150.190... 172.21.150.190...
+     rdp-tcp#2         muoadmin_tg               3  Active
+    cmd exited on 172.21.150.190 with error code 1.
+    */
 
- SESSIONNAME       USERNAME ID  STATE TYPE        DEVICE
->services                                    0  Disc
- console                                     1  Conn
-                   muoadmin_tg               2  Disc
- rdp-tcp#2         muoadmin_tg               3  Active
- rdp-tcp                                 65536  Listen
-cmd exited on 172.21.150.190 with error code 1.
-
-D:\SysinternalsSuite_20181210>
-
-
-
-
-
-
+        static ClassLibrary.CommonLibrary lib = new ClassLibrary.CommonLibrary();
 
 
         static void Main(string[] args) {
+            string PsExecPath = lib.LoadINI("PsExecPath", @"\RemoteExec.ini");
+            if (PsExecPath == null)
+                lib.Terminate("No argument(PsExec)!!!");
+            if (!File.Exists(PsExecPath))
+                lib.Terminate("PsExec is not exist!!!");
+
+            string UpdatePath = lib.LoadINI("UpdatePath", @"\RemoteExec.ini");
+            if (UpdatePath != null)
+                if (lib.CheckUpdate("RemoteExec", UpdatePath))
+                    lib.DoUpdate("RemoteExec", UpdatePath);
+
+
+
+
+
+
+
+
+
+        }
+
+
+        static ArrayList SplitSessionResult(string Result) {
+
+            
         }
     }
 }
